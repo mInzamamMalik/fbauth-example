151ca1492fd5910a84de6a64256f01153d853d95
asifasif <globalstar93@gmail.com>
2015-07-05 01:32:46 -0700

    user name get

--- app.js
+++ app.js
@@ -13,16 +13,22 @@ function abc(){
     var ref = new Firebase("https://malikasinger-fbauth.firebaseio.com");
 
     this.login = function(){
-
         ref.authWithOAuthPopup("facebook", function(error, authData) {
             if (error) {
                 console.log("Login Failed!", error);
             } else {
                 console.log("Authenticated successfully with payload:", authData);
+
+                var name = authData.facebook.cachedUserProfile.name;
             }
         });
-
+        this.name = name;
     };
+    this.logout = function(){
+        ref.unauth();
+    }
+
+
 
 
 
